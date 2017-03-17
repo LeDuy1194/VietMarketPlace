ec82f634e7bedcab12096ff973019d5f0fe98051
LeDuy <leduy211194@gmail.com>
2017-03-17 15:41:42 +0700

    Add functions.php in lib

--- app/Http/Controllers/Admin/CateController.php
+++ app/Http/Controllers/Admin/CateController.php
@@ -20,7 +20,7 @@ class CateController extends Controller
     public function postAdd (CateRequest $request) {
     	$cate = new Cate;
     	$cate->name = $request->txtCateName;
-    	$cate->alias = $request->txtCateName;
+    	$cate->alias = changeTitle($request->txtCateName);
     	$cate->order = $request->txtOrder;
     	$cate->parent_id = 1;
     	$cate->save();
