dba260ee15b78ecc420a4a819c0f61949c9cc2fb
LeDuy <leduy211194@gmail.com>
2017-05-08 13:38:04 +0700

    fix bugs matching, suggest.

--- app/Models/OrderTag.php
+++ app/Models/OrderTag.php
@@ -10,7 +10,19 @@ class OrderTag extends Model
 
     public $timestamps = true;
 
+    /*	return array of tag_id;
+    */
     public function getTagByOrderId($id) {
-    	return $this->select('tag_id')->where('order_id',$id)->orderBy('tag_id','asc')->get();
+    	$temp = $this->select('tag_id')->where('order_id',$id)->orderBy('tag_id','asc')->get();
+    	$result = [];
+    	if (!is_null($temp)) {
+	    	foreach ($temp as $tag) {
+	    		$result[] = $tag->tag_id;
+	    	}
+	    }
+	    else {
+	    	$result = NULL;
+	    }
+    	return $result;
     }
 }
--- app/Models/StockTag.php
+++ app/Models/StockTag.php
@@ -10,7 +10,19 @@ class StockTag extends Model
 
     public $timestamps = true;
 
+    /*	return array of tag_id;
+    */
     public function getTagByStockId($id) {
-    	return $this->select('tag_id')->where('stock_id',$id)->orderBy('tag_id','asc')->get();
+    	$temp = $this->select('tag_id')->where('stock_id',$id)->orderBy('tag_id','asc')->get();
+    	$result = [];
+    	if (!is_null($temp)) {
+	    	foreach ($temp as $tag) {
+	    		$result[] = $tag->tag_id;
+	    	}
+	    }
+	    else {
+	    	$result = NULL;
+	    }
+    	return $result;
     }
 }
