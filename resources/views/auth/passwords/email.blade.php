19feb35010a013653e4c133b7355c0544c07390b
LeDuy <leduy211194@gmail.com>
2017-05-16 20:52:46 +0700

    update forget password(full).

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,31 +1,27 @@
 @extends('layouts.master')
 @section('content')
 <div class="row">
-	<div class="col-md-6">
-		<div class="card">
-			{{-- <form role="form" action="{!!url('password/email')!!}" method="POST">
-				<input type="hidden" name="_token" value="{!!csrf_token()!!}"/>
-				<h3 class="card-header">Khôi phục Mật khẩu</h3>
-				<div class="card-block">
-					<div class="form-group">
-						<label for="email">Email: </label>
-						<input type="email" class="form-control" placeholder="Địa chỉ Email" id="email" name="email"/>
-					</div>
-					<div class="form-group">
-						<button type="submit" class="btn btn-lg btn-account">Khôi phục mật khẩu.</button>
-					</div>
-				</div>
-			</form> --}}
+  <div class="col-md-6">
+    <div class="card">
+      @if(session('status'))
+        <div class="alert alert-success">
+          {{ session('status') }}
+        </div>
+      @endif
+      @include('errors.input')
+      <br>
+      <h3 class="card-header">Reset Password</h3>
+      <div class="card-block">
+        {!! Form::open(['url' => 'password/email', 'method' => "POST"]) !!}
 
-			<h3 class="card-header">Reset Password</h3>
-			<div class="card-block">
-				{!! Form::open(['url' => 'password/email', 'method' => "POST"]) !!}
-				{{ Form::label('email', 'Email:') }}
-				{{ Form::email('email', null, ['class' => 'form-control']) }}
-				{{ Form::submit('Gửi', ['class' => 'btn btn-primary']) }}
-				{{ Form::close() }}
-			</div>
-		</div>
-	</div>
+        {{ Form::label('email', 'Email:') }}
+        {{ Form::email('email', null, ['class' => 'form-control']) }}
+
+        {{ Form::submit('Gửi', ['class' => 'btn btn-primary']) }}
+
+        {{ Form::close() }}
+      </div>
+    </div>
+  </div>
 </div>
 @endsection()
